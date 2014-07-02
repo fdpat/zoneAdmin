8df6328e3f77a885fceeef98f34bf05ef9fa15da
fdpat <fdpat@users.noreply.github.com>
2014-07-02 16:15:16 -0400

    T

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Bootstrap -->
+        <link href="libs/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
+        
+    </head>
+    <body>
+        
+    </body>
+</html>
