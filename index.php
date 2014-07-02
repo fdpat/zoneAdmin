d9a658f2fead88f4d3cad9a602f319da9d3bea35
fdpat <fdpat@users.noreply.github.com>
2014-07-02 16:25:02 -0400

    Index

--- index.php
+++ index.php
@@ -9,9 +9,35 @@
         
         <!-- Bootstrap -->
         <link href="libs/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
-        
+       
+       <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+        <script src="js/jquery-2.1.1.min.js"></script> 
+        <script src="libs/bootstrap/js/bootstrap.js"></script> 
+
     </head>
     <body>
+        <div class="site-wrapper">
+
+            <div class="site-wrapper-inner">
+
+                <div class="cover-container">
+                
+                    <div class="inner cover">
+                        <h1 class="cover-heading">Connexion</h1>
+                        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                        <p class="lead">
+                            <a href="#" class="btn btn-lg btn-default">Se connecter</a>
+                        </p>
+                    </div>
+                    
+                    <div class="mastfoot">
+                        <div class="inner">
+                            <p>Créé par <a href="http://fdcanada.ca">Fd Canada</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
     </body>
 </html>
